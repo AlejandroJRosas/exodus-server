ffc0984c877e7b83464c250eefa3c28d06434913
Alejandro Rosas <61267550+AlejandroJRosas@users.noreply.github.com>
2025-01-13 01:27:51 -0400

    feat: add classifier

--- analizer.py
+++ analizer.py
@@ -1,2 +1,6 @@
+from transformers import pipeline
+
+classifier = pipeline('text-classification', model="sentiment_roberta_model", tokenizer="sentiment_roberta_model")
+
 def infer_feeling(text):
-  return text+"🤖"
+  return classifier(text)
